c3e6859c0b901e1be2ca3291292f063857c168b2
Dyana Wu <dyanawu@gmail.com>
2020-06-26 12:58:43 +0800

    Add full_title helper
    - Add full title helper
    - Update layout to use helper
    - Update homepage to only use base title
    - Update test for homepage title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Full page title helper
+  def full_title(page_title = "")
+    base_title = "RoR Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
