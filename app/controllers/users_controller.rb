f312094907769d815f44d9d2a5142c0816e29d18
Dyana Wu <dyanawu@gmail.com>
2020-06-30 01:39:51 +0800

    Log users in on signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      reset_session
+      log_in @user
     else
       render 'new'
     end
