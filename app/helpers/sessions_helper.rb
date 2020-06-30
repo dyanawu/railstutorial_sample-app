57eaff0e650f82e8adda89abc58549790adbeb87
Dyana Wu <dyanawu@gmail.com>
2020-06-30 16:58:54 +0800

    Use session token when setting current user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,10 +11,11 @@ module SessionsHelper
 
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
+      user = User.find_by(id: user_id)
+      @current_user ||= user if session[:session_token] == user.session_token
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user&.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
       end
