12c4613b5d6ab86462e547a315e27c1ed650e983
Dyana Wu <dyanawu@gmail.com>
2020-07-01 16:13:12 +0800

    Make seed and test users activated

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,9 @@ User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 puts "added main example user"
 
 # Generate a bunch of additional users.
@@ -14,6 +16,8 @@ puts "added main example user"
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
   puts "added #{email}"
 end
