e015903e90e1b04ce1e92079d273d166894640c4
Dyana Wu <dyanawu@gmail.com>
2020-07-01 17:00:44 +0800

    Update test for login with remember
    - convert sessions#create to use instance variable
    - update test to check for specific user remember_token cookie instead of generically checking for non-empty remember_token cookie

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      if user.activated?
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      if @user.activated?
         forwarding_url = session[:forwarding_url]
         reset_session
-        log_in user
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_to forwarding_url || user
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_to forwarding_url || @user
       else
         message  = "Account not activated. "
         message += "Check your email for the activation link."
