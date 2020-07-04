ca7b20724abf61f2e020afee2cb076dd54fdbea9
Dyana Wu <dyanawu@gmail.com>
2020-07-04 17:49:27 +0800

    Populate feed items for homepage when failing to save micropost

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,6 +7,7 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      @feed_items = current.user.feed.paginate(page: params[:page])
       render 'static_pages/home'
     end
   end
