18f07420fc87362a8b7cb106cbfae79cfe1ca9cd
Dyana Wu <dyanawu@gmail.com>
2020-07-05 02:44:48 +0800

    Add host option to mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,12 +61,15 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "sample_app_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = {
+    :host => 'dwu-sample.herokuapp.com'
+  }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'dwu-sample.heroku.com',
+    :domain         => 'dwu-sample.herokuapp.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
